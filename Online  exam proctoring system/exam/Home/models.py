19fd12627cf986578f32f1ba713b4a56e3bde909
SooryanshSingh <sooryanshsingh56@gmail.com>
2024-05-24 00:37:05 +0530

    making home ui better and added views.py for crud operations partially

--- Online  exam proctoring system/exam/Home/models.py
+++ Online  exam proctoring system/exam/Home/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Exam(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()        
-    duration = models.IntegerField() #duration of the test 
+    duration = models.IntegerField() 
     company = models.ForeignKey(User, on_delete=models.CASCADE, related_name='exams')
 
 class Question(models.Model):
@@ -15,9 +15,7 @@ class Question(models.Model):
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
     text = models.TextField()
-    #for mcq we should have bool whether option is correct
     is_correct = models.BooleanField(default=False)
-#a feedback model for gibving feedback after test ends
 class Feedback(models.Model):
     exam = models.ForeignKey(Exam, on_delete=models.CASCADE, related_name='feedbacks')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- Online  exam proctoring system/exam/Home/views.py
+++ Online  exam proctoring system/exam/Home/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render, redirect
+from .models import Exam,Exam, Question, Answer
+from django import forms
+from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import CustomUserCreationForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .forms import QuestionForm, AnswerForm
 
 def home(request):
     return render(request, 'home.html')
@@ -10,6 +15,8 @@ def about(request):
     return render(request, 'about.html')
 
 def login_user(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('home'))                           
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
@@ -26,6 +33,8 @@ def logout_user(request):
     return redirect('home')
 
 def signup(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('home'))                           
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
@@ -40,4 +49,65 @@ def signup(request):
                 return redirect('signup')
     else:
         form = CustomUserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+        return render(request, 'signup.html', {'form': form})
+
+
+def exam_create(request):
+    if request.method == 'POST':
+        form = forms.ModelForm(request.POST)
+        if form.is_valid():
+            exam = form.save(commit=False)
+            exam.company = request.user
+            exam.save()
+            return redirect('', exam_id=exam.pk)
+    else:
+        form = forms.ModelForm()
+    return render(request, '', {'form': form})
+
+def exam_update(request, exam_id):
+    exam = get_object_or_404(Exam, pk=exam_id, company=request.user)
+    if request.method == 'POST':
+        form = forms.ModelForm(request.POST, instance=exam)
+        if form.is_valid():
+            exam = form.save(commit=False)
+            exam.company = request.user
+            exam.save()
+            return redirect('', exam_id=exam.pk)
+    else:
+        form = forms.ModelForm(instance=exam)
+    return render(request, '', {'form': form})
+
+
+def question_create(request, exam_id):
+    exam = get_object_or_404(Exam, pk=exam_id, company=request.user)
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.exam = exam
+            question.save()
+            return redirect('', exam_id=exam_id)
+    else:
+        form = QuestionForm()
+    return render(request, '', {'form': form, 'exam': exam})
+
+def question_update(request, exam_id, question_id):
+    exam = get_object_or_404(Exam, pk=exam_id, company=request.user)
+    question = get_object_or_404(Question, pk=question_id, exam=exam)
+    if request.method == 'POST':
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            form.save()
+            return redirect('', exam_id=exam_id)
+    else:
+        form = QuestionForm(instance=question)
+    return render(request, '', {'form': form, 'exam': exam})
+
+def question_delete(request, exam_id, question_id):
+    exam = get_object_or_404(Exam, pk=exam_id, company=request.user)
+    question = get_object_or_404(Question, pk=question_id, exam=exam)
+    if request.method == 'POST':
+        question.delete()
+        return redirect('exam_detail', exam_id=exam_id)
+    return render(request, '', {'question': question, 'exam': exam})
+
